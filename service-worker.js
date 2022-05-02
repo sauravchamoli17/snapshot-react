10b82518577f4bb19c2747e6fa19ce6440e2b1f3
Saurav Chamoli <saurav.chamoli@ebizondigital.com>
2022-05-02 14:13:56 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github.com/sauravchamoli17/snapshot-react-app/precache-manifest.ca23193414dc09101676e57951cb2654.js"
+  "/sauravchamoli17/snapshot-react/precache-manifest.ce05d0234098d9b3c2692eab664e21a0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.com/sauravchamoli17/snapshot-react-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sauravchamoli17/snapshot-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
