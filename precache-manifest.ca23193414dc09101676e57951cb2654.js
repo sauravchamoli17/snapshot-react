f8b3dbc2e4cc84b03e3fd00447482197f790c254
Saurav Chamoli <saurav.chamoli@ebizondigital.com>
2022-05-02 14:09:32 +0530

    Updates

---
+++ precache-manifest.ca23193414dc09101676e57951cb2654.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "943d2d7821b3a9c1bf54fd987ce076e1",
+    "url": "/github.com/sauravchamoli17/snapshot-react-app/index.html"
+  },
+  {
+    "revision": "d9ae4cc3446ef58175d3",
+    "url": "/github.com/sauravchamoli17/snapshot-react-app/static/css/main.d03163bd.chunk.css"
+  },
+  {
+    "revision": "bf7550e09bb8dfb65f3b",
+    "url": "/github.com/sauravchamoli17/snapshot-react-app/static/js/2.259ad13e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github.com/sauravchamoli17/snapshot-react-app/static/js/2.259ad13e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9ae4cc3446ef58175d3",
+    "url": "/github.com/sauravchamoli17/snapshot-react-app/static/js/main.d9240029.chunk.js"
+  },
+  {
+    "revision": "24f5aa4c696a9864b93a",
+    "url": "/github.com/sauravchamoli17/snapshot-react-app/static/js/runtime-main.ba8bb2e9.js"
+  }
+]);
